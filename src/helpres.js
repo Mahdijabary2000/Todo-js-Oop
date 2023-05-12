00816ccce6a2c63126dd725c66e4cb5a85f57c4d
mahdi <125316110+Mahdijabary2000@users.noreply.github.com>
2023-05-13 01:00:58 +0430

    update style(scss)

--- src/helpres.js
+++ src/helpres.js
@@ -4,20 +4,20 @@ export function createTaskDiv(text, id, deleteCallback, editCallback) {
     taskDiv.setAttribute("id", id);
   //creat text todo
     const taskText = document.createElement("span");
-    taskText.classList.add("fw-bold");
+    taskText.classList.add("fw-bold","textTodo");
     taskText.innerHTML = text;
   
     const options = document.createElement("span");
     options.classList.add("options");
   //add editIcon to div
     const editIcon = document.createElement("i");
-    editIcon.classList.add("fas", "fa-edit");
+    editIcon.classList.add("fas", "fa-edit","edit");
     editIcon.setAttribute("data-bs-toggle", "modal");
     editIcon.setAttribute("data-bs-target", "#form");
     editIcon.addEventListener("click", () => editCallback(id));
   //add deleteIcon to div
     const deleteIcon = document.createElement("i");
-    deleteIcon.classList.add("fas", "fa-trash-alt");
+    deleteIcon.classList.add("fas", "fa-trash-alt","trash");
     deleteIcon.addEventListener("click", () => deleteCallback(id));
   //add options to boxTodo
     options.appendChild(editIcon);
